3032adf19a15a4c294cc1601ddc5d574e8f6918e
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-07-23 21:04:01 -0500

    Merge commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,11 +7,13 @@ import Browse from "./components/Browse";
 import Features from "./components/Features";
 import MoreInfo from "./components/Moreinfo";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NewProjectForm from "./components/NewProjectForm";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <NewProjectForm />
       <Router>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
--- my-app/src/components/Signup.js
+++ my-app/src/components/Signup.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
 const SignUp = () => {
     return(
         <div className = 'App'>
             <h1>Sign Up</h1>
+            
         </div>
 
     )
